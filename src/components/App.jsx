83e550f8b130333647975fefbc059a99a2112390
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-09-27 20:20:03 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
+import Notiflix from 'notiflix';
+import { useState, useEffect } from 'react';
+import { ContactList } from './Contacts/Contacts';
+import { ContactForm } from './Form/Form';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
+  const handleSubmit = newContact => {
+    const existingContact = contacts.find(
+      contact => contact.name === newContact.name
+    );
+    if (existingContact) {
+      // alert(`${existingContact.name} is already in contacts`);
+      Notiflix.Notify.failure(`${existingContact.name} is already in contacts`);
+    } else {
+      const updatedContacts = [...contacts, newContact];
+      setContacts(updatedContacts);
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+    }
+  };
+
+  const filterContacts = e => {
+    setFilter(e.target.value);
+  };
+
+  const deleteContact = e => {
+    const id = e.currentTarget.id;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    const updatedContacts = filteredContacts.filter(
+      contact => contact.id !== id
+    );
+    setContacts(updatedContacts);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={handleSubmit}></ContactForm>
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={filterContacts}></Filter>
+      <ContactList
+        contacts={filteredContacts}
+        onButtonClick={deleteContact}
+      ></ContactList>
     </div>
   );
 };
